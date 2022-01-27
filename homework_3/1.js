3c891048b1613cff97cd835378feda0517396ee6
a-kolodiazhniy <92212254+a-kolodiazhniy@users.noreply.github.com>
2022-01-27 15:18:46 +0200

    Update 1.js

--- homework_3/1.js
+++ homework_3/1.js
@@ -2,7 +2,7 @@ const arr = [1, 2, 3];
 
 function forEach(arr, callback) {
     if (!Array.isArray(arr)) throw new Error('Error: 1st argument must be an array');
-    if (typeof callback !== 'function') throw new Error('Error: 2st argument must be an function');
+    if (typeof callback !== 'function') throw new Error('Error: 2nd argument must be an function');
 
     for (let i = 0; i < arr.length; i++) callback(arr[i], i, arr);
 }
